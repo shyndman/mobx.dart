fb3378228b84cad3af3e80504e4c6575efff3c00
Vadim Tsushko <vadimtsushko@gmail.com>
2019-02-17 17:33:07 +0500

    Move angular examples outside angular_mobx repo. Basic tests for angular_mobx added

---
+++ angular_mobx_examples/counter/store/lib/src/counter.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:mobx/mobx.dart';
+
+part 'counter.g.dart';
+
+Counter counterSingleton = Counter();
+
+class Counter = CounterBase with _$Counter;
+
+abstract class CounterBase implements Store {
+  Timer timer;
+  @observable
+  int value = 0;
+
+  @action
+  void increment() {
+    value++;
+  }
+
+  @action
+  void decrement() {
+    value--;
+  }
+
+  startTimer() {
+    stopTimer();
+    timer = Timer.periodic(Duration(seconds: 1), (_) => increment());
+  }
+  
+  stopTimer() {
+    if (timer != null) {
+      timer.cancel();
+      timer = null;
+    }
+  }
+}
