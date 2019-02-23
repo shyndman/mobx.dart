7353dffc9596016ed778827b0be23315bbac0f06
Vadim Tsushko <vadimtsushko@gmail.com>
2019-02-23 17:07:49 +0500

    Tests for mobxAutorun and mobxReactive directives

--- angular_mobx/lib/src/mobx_reaction_directive.dart
+++ angular_mobx/lib/src/mobx_reaction_directive.dart
@@ -6,14 +6,13 @@ import 'mobx_autorun_directive.dart';
 @Directive(selector: '[mobxReaction]')
 class MobxReactionDirective extends MobxAutorunDirective {
   dynamic Function(Reaction) _reaction;
-  ChangeDetectorRef detectorRef;
 
   @Input()
   set mobxReaction(dynamic Function(Reaction) value) {
     _reaction = value;
   }
 
-  MobxReactionDirective(TemplateRef templateRef, ViewContainerRef viewContainer, this.detectorRef )
+  MobxReactionDirective(TemplateRef templateRef, ViewContainerRef viewContainer)
       : super(templateRef, viewContainer);
   void createReaction() {
     dispose = reaction(_reaction, (_) {
---
+++ angular_mobx/test/components/test_reaction_component.dart
@@ -0,0 +1,27 @@
+import 'package:angular/angular.dart';
+import '../stores/test_store.dart';
+import 'package:angular_mobx/angular_mobx.dart';
+
+@Component(
+  selector: 'my-app',
+  template: '''
+<div *mobxReaction="testChanges">
+  <span id="fullname">{{store.fullName}}</span>
+</div>
+<button id="setLastNameBtn" (click)="setLastName()">Set Name</button>
+<button id="unrelatedBtn" (click)="doSomethingUnrelated()">Do something unrelated</button>
+
+  ''',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  directives: [MobxReactionDirective],
+)
+class TestReactionComponent {
+  var store = globalStore;
+
+  void setLastName() {
+    store.setNames(store.firstName, 'Dean');
+  }
+
+  doSomethingUnrelated() {}
+  testChanges(_)=>store.fullName;
+}
