af2481a6f01542d34b61f377c57389a52d29d088
Vadim Tsushko <vadimtsushko@gmail.com>
2019-02-24 14:57:33 +0500

    Angular todos iteration

--- angular_mobx_examples/todos/angular_todos/lib/src/section/section_component.dart
+++ angular_mobx_examples/todos/angular_todos/lib/src/section/section_component.dart
@@ -2,10 +2,12 @@ import 'package:angular/angular.dart';
 import 'package:angular_mobx/angular_mobx.dart';
 import 'package:store/store.dart';
 
+import '../todo_component/todo_component.dart';
+
 @Component(
   selector: 'app-section',
   templateUrl: 'section_component.html',
-  directives: [NgFor, NgIf, MobxAutorunDirective]
+  directives: [NgFor, NgIf, MobxAutorunDirective, TodoItemComponent]
 )
 class SectionComponent {
   TodoList todos;
